eb44d46c089961b303fd791fb0de2c24ac3e8af3
Yulianto W <sehari24jam@gmail.com>
2017-09-24 12:34:36 +0700

    up according example

--- ybot.go
+++ ybot.go
@@ -1,10 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
-  fmt.Printf("Going to start\n")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("YBOTTOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Printf("Starting read")
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.Document != nil {
+			log.Printf("Going to use getfiledirecturl")
+			//file, err := bot.GetFile(update.Message.Document)
+			fn, err := bot.GetFileDirectURL(update.Message.Document.FileID)
+			if err != nil {
+				log.Printf("File: %s", fn)
+			}
+
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 
 }
-
-
