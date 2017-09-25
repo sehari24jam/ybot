dad1de0aad0b383e15173d8a4e4cde302f90ae20
Yulianto W <sehari24jam@gmail.com>
2017-09-25 10:08:38 +0700

    zip worked

--- ybot.go
+++ ybot.go
@@ -4,11 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
@@ -16,6 +18,7 @@ import (
 
 var webhook = flag.Bool("webhook", false, "webhook mode")
 var debug = flag.Bool("debug", false, "debug")
+var noisy = flag.Bool("noisy", false, "noisy")
 var token = flag.String("token", "", "token")
 var pubip = flag.String("pubip", "", "public ip, get with 'curl -s https://ipinfo.io/ip'")
 var port = flag.Int("port", 8443, "webhook server port")
@@ -27,21 +30,25 @@ func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	if update.Message == nil {
 		return
 	}
+	Noisy := *noisy
+	if update.Message.From.UserName == "sehari24jam" {
+		Noisy = true
+	}
 
 	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID,
-		"Send me asciidoc file (.adoc). I don't understand: "+update.Message.Text)
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Failed")
 	msg.ReplyToMessageID = update.Message.MessageID
 
 	switch update.Message.Text {
 	case "/start":
-		msg.Text = fmt.Sprintf("Welcome %s (%s %s). You may send me asciidoc (.adoc) file",
+		msg.Text = fmt.Sprintf("Welcome %s (%s %s).\nYou may send me asciidoc (.adoc) file",
 			update.Message.From.UserName, update.Message.From.FirstName, update.Message.From.LastName)
 		bot.Send(msg)
 		return
 	default:
 		if update.Message.Document == nil {
+			msg.Text = "Send me asciidoc file (.adoc). I don't understand: " + update.Message.Text
 			bot.Send(msg)
 			return
 		}
@@ -51,43 +58,74 @@ func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	//log.Printf("DocFile: %s", update.Message.Document.FileName)
 	if err != nil {
 		log.Fatal(err)
-		msg.Text = "Failed to proceed uploaded file"
+		if Noisy {
+			msg.Text = "Failed to proceed uploaded file"
+		}
 		bot.Send(msg)
 		return
 	}
 
-	lfolder := path.Join("/tmp", path.Dir(f.FilePath))
-	//lfile := path.Join("/tmp", f.FilePath)
 	ext := path.Ext(update.Message.Document.FileName)
-	pdffile := path.Join(lfolder, strings.TrimSuffix(update.Message.Document.FileName, ext)+".pdf")
-	adocfile := path.Join(lfolder, update.Message.Document.FileName)
+	tmp := path.Join(os.TempDir(), "ybot."+update.Message.Chat.UserName)
+	zipped := false
+	switch strings.ToLower(ext) {
 
-	if ext != ".adoc" {
+	case ".zip":
+		zipped = true
+		tmp, err = ioutil.TempDir("", "ybot-")
+		if err != nil {
+			log.Fatal(err)
+			log.Print(os.RemoveAll(tmp))
+			if Noisy {
+				msg.Text = "Unable to create temp"
+			}
+			bot.Send(msg)
+			return
+		}
+		msg.Text = "Looks good, let me work on this zip file"
+		bot.Send(msg)
+
+	case ".adoc":
+		msg.Text = "Looks good, let me work on this file"
+		bot.Send(msg)
+
+	default:
 		msg.Text = "Document is not an adoc"
 		bot.Send(msg)
-		return
 	}
+	workfolder := path.Join(tmp, path.Dir(f.FilePath))
+	//lfile := path.Join("/tmp", f.FilePath)
+	pdffile := path.Join(workfolder, strings.TrimSuffix(update.Message.Document.FileName, ext)+".pdf")
+	workfile := path.Join(workfolder, update.Message.Document.FileName)
 
-	msg.Text = "Looks good, let me work on this file"
-	bot.Send(msg)
-
+	// get WorkFile from TG
 	response, err := http.Get(f.Link(*token))
 	if err != nil {
-
 		log.Fatal(err)
-		msg.Text = "Failed to get uploaded file"
+		if zipped {
+			log.Print(os.RemoveAll(tmp))
+		}
+		if Noisy {
+			msg.Text = "Failed to get uploaded file"
+		}
 		bot.Send(msg)
 		return
-
 	}
 	defer response.Body.Close()
 
-	os.MkdirAll(lfolder, os.ModePerm)
+	// create sub folder as necessary
+	os.MkdirAll(workfolder, os.ModePerm)
 
-	file, err := os.Create(adocfile)
+	// save WorkFile
+	file, err := os.Create(workfile)
 	if err != nil {
 		log.Fatal(err)
-		msg.Text = "Unable to create new file"
+		if zipped {
+			log.Print(os.RemoveAll(tmp))
+		}
+		if Noisy {
+			msg.Text = "Unable to create new file"
+		}
 		bot.Send(msg)
 		return
 	}
@@ -96,25 +134,82 @@ func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	file.Close()
 	if err != nil {
 		log.Fatal(err)
-		msg.Text = "Unable to buffer uploaded file"
+		if zipped {
+			log.Print(os.RemoveAll(tmp))
+		}
+		if Noisy {
+			msg.Text = "Unable to buffer uploaded file"
+		}
 		bot.Send(msg)
 		return
 	}
 
 	go func() {
-		cmd := exec.Command("Ad", adocfile)
-		cmd.Dir = lfolder
+		if zipped {
+			cmd := exec.Command("7z", "x", workfile)
+			cmd.Dir = workfolder
+			out, err := cmd.Output()
+			if err != nil {
+				log.Fatal(err)
+				if zipped {
+					log.Print(os.RemoveAll(tmp))
+				}
+				if Noisy {
+					msg.Text = fmt.Sprintf("Failed %v\n%v", out, err)
+				}
+				bot.Send(msg)
+				return
+			}
+			workfile = "*.adoc"
+		}
+		cmd := exec.Command("Ad", workfile)
+		cmd.Dir = workfolder
 		out, err := cmd.Output()
 		if err != nil {
 			log.Fatal(err)
-			msg.Text = fmt.Sprintf("Fail %v\n%v", out, err)
-		} else if len(out) > 0 {
-			msg.Text = fmt.Sprintf("Success %v", out)
-		} else {
-			msg.Text = fmt.Sprintf("Success")
+			if zipped {
+				log.Print(os.RemoveAll(tmp))
+			}
+			if Noisy {
+				msg.Text = fmt.Sprintf("Failed %v\n%v", out, err)
+			}
+			bot.Send(msg)
+			return
 		}
-		bot.Send(msg)
-		bot.Send(tgbotapi.NewDocumentUpload(msg.ChatID, pdffile))
+
+		if zipped {
+			files, err := filepath.Glob(path.Join(workfolder, "*.pdf"))
+			if err != nil {
+				log.Fatal(err)
+				if zipped {
+					log.Print(os.RemoveAll(tmp))
+				}
+				if Noisy {
+					msg.Text = fmt.Sprintf("Failed %v", err)
+				}
+				bot.Send(msg)
+				return
+			}
+
+			for _, f := range files {
+				bot.Send(tgbotapi.NewDocumentUpload(msg.ChatID, f))
+			}
+			if Noisy {
+				msg.Text = fmt.Sprintf("Success %v", out)
+			} else {
+				msg.Text = "Success"
+			}
+			bot.Send(msg)
+			log.Print(os.RemoveAll(tmp))
+		} else {
+			if Noisy {
+				msg.Text = fmt.Sprintf("Success %v", out)
+			} else {
+				msg.Text = "Success"
+			}
+			bot.Send(msg)
+			bot.Send(tgbotapi.NewDocumentUpload(msg.ChatID, pdffile))
+		}
 
 	}()
 
@@ -151,6 +246,7 @@ func main() {
 		log.Printf("Starting Collect Update from WebHook")
 		for update := range updates {
 			handleUpdate(bot, update)
+			//log.Printf("%+v\n", update)
 		}
 
 	} else {
